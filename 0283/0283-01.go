001a332b9198f63203a3920451367b5a79107432
Cass Hsu <cass.hsu@gmail.com>
2021-01-20 23:27:15 +0800

    init 283 & Approach 1

---
+++ 0283/0283-01.go
@@ -0,0 +1,11 @@
+func moveZeroes(nums []int)  {
+    p := 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] != 0 {
+            if i != p {
+                nums[i], nums[p] = nums[p], nums[i]
+            }
+            p++
+        }
+    }
+}
