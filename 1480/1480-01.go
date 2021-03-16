c8697a71d9d01797827bf9e7bc2220d3074656e7
CassHsu <cass.hsu@gmail.com>
2021-03-16 11:53:25 +0800

    Create 1480-01.go

---
+++ 1480/1480-01.go
@@ -0,0 +1,8 @@
+func runningSum(nums []int) []int {
+    ret := make([]int, len(nums))
+    ret[0] = nums[0]
+    for i := 1; i < len(nums); i++ {
+        ret[i] = ret[i-1] + nums[i]
+    }
+    return ret
+}
