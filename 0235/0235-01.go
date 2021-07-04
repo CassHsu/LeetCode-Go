f77afec30e624afe473dc9da6ba346d421dc72e6
CassHsu <cass.hsu@gmail.com>
2021-07-04 18:04:55 +0800

    Approach 1: Recursive

---
+++ 0235/0235-01.go
@@ -0,0 +1,11 @@
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    if p.Val > root.Val && q.Val > root.Val {
+        return lowestCommonAncestor(root.Right, p, q)
+        
+    } else if p.Val < root.Val && q.Val < root.Val {
+        return lowestCommonAncestor(root.Left, p, q)
+        
+    } else {
+        return root
+    }
+}
