a13b844cfd95a046c30927dff8bea6a7e1aa8a10
CassHsu <cass.hsu@gmail.com>
2021-07-05 20:35:57 +0800

    Approach 2: Iterative

---
+++ 0235/0235-02.go
@@ -0,0 +1,15 @@
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    for root != nil {
+        if p.Val > root.Val && q.Val > root.Val {
+            root = root.Right
+            
+        } else if p.Val < root.Val && q.Val < root.Val {
+            root = root.Left
+            
+        } else {
+            return root
+        }
+    }
+    
+    return nil
+}
