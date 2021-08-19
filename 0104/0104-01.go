0e0ade65b3fb31e6c2bf8e9b5496ae3fc11fd647
CassHsu <cass.hsu@gmail.com>
2021-08-19 18:22:16 +0800

    Create 0104-01.go

---
+++ 0104/0104-01.go
@@ -0,0 +1,14 @@
+func maxDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    
+    l := maxDepth(root.Left)
+    r := maxDepth(root.Right)
+    
+    if l > r {
+        return l + 1
+    } else {
+        return r + 1
+    }
+}
