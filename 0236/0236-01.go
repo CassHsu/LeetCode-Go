e4415170e96915995e390ff4797fc47eac13fce3
CassHsu <cass.hsu@gmail.com>
2021-09-15 09:38:30 +0800

    Create 0236-01.go

---
+++ 0236/0236-01.go
@@ -0,0 +1,16 @@
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    if root == nil { return nil }
+    if root == p || root == q { return root }
+    
+    l := lowestCommonAncestor(root.Left,  p, q)
+    r := lowestCommonAncestor(root.Right, p, q)
+    
+    if l != nil && r != nil { return root }
+    if l == nil && r == nil { return nil }
+    
+    if l == nil {
+        return r
+    } else {
+        return l
+    }
+}
