b838b0cc1bc250206937ed5ddb0ad9e610216ba5
CassHsu <cass.hsu@gmail.com>
2021-09-15 09:33:56 +0800

    Recursion

---
+++ 0116/0116-01.go
@@ -0,0 +1,15 @@
+func connect(root *Node) *Node {
+    if root == nil { return nil }
+    
+    if root.Left != nil {
+        root.Left.Next = root.Right
+        
+        if root.Next != nil {
+            root.Right.Next = root.Next.Left
+        }
+    }
+    
+    connect(root.Left)
+    connect(root.Right)
+    return root
+}
