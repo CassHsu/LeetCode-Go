a7b05be7016d654e1bed90d4daea0e39fc3e2e71
CassHsu <cass.hsu@gmail.com>
2021-09-19 11:55:32 +0800

    faster

--- 0118/0118-01.go
+++ 0118/0118-01.go
@@ -1,16 +1,18 @@
 func generate(numRows int) [][]int {
-    ret := make([][]int, 0)
+    ans := [][]int{}
     
     for i := 0; i < numRows; i++ {
-        row := make([]int, i+1)
+        row := make([]int, i + 1)
+        
         row[0] = 1
+        row[i] = 1
         
         for j := 1; j < i; j++ {
-            row[j] = ret[i-1][j-1] + ret[i-1][j]
+            row[j] = ans[i - 1][j] + ans[i - 1][j - 1]
         }
         
-        row[i] = 1
-        ret = append(ret, row)
+        ans = append(ans, row)
     }
-    return ret
+    
+    return ans
 }
