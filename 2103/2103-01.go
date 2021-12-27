6922ac97d1180188597f38842c9b57a3e4358ea1
CassHsu <cass.hsu@gmail.com>
2021-12-27 22:08:17 +0800

    Map

---
+++ 2103/2103-01.go
@@ -0,0 +1,32 @@
+func countPoints(rings string) int {
+    count := 0
+    m := map[byte]string {}
+    
+    for r, c := 0, 1; c < len(rings); r, c = r + 2, c + 2 {
+        k := rings[c]
+        v, ok := m[k]
+        if !ok { 
+            m[k] = ""
+        }
+        m[k] = v + string(rings[r])
+    }
+    
+    for _, v := range m {
+        r := false
+        g := false
+        b := false
+        
+        for _, c := range v {
+            if string(c) == "R" { r = true }
+            if string(c) == "G" { g = true }
+            if string(c) == "B" { b = true }
+            
+            if r && g && b {
+                count++
+                break
+            }
+        }
+    }
+    
+    return count
+}
