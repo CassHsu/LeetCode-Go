7d9e6b773c4c0f41c4acd061763830272469140d
CassHsu <cass.hsu@gmail.com>
2022-10-13 13:48:01 +0800

    Sorting

---
+++ 0976/0976-01.go
@@ -0,0 +1,11 @@
+func largestPerimeter(nums []int) int {
+    sort.Ints(nums)
+    
+    for i := len(nums) - 3; i > -1; i-- {
+        if nums[i] + nums[i + 1] > nums[i + 2] {
+            return nums[i] + nums[i + 1] + nums[i + 2]
+        }
+    }
+    
+    return 0
+}
