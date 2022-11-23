f65ad39490af82095a0b90811a05cfed77455700
CassHsu <cass.hsu@gmail.com>
2022-11-23 21:09:42 +0800

    Create 1185-01.go

---
+++ 1185/1185-01.go
@@ -0,0 +1,9 @@
+import (
+    "fmt"
+    "time"
+)
+
+func dayOfTheWeek(day int, month int, year int) string {
+    t := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.Local)
+    return fmt.Sprintf("%s", t.Weekday())
+}
