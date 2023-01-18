0d1ed00ab3fbe5f926769854e1cb35512e18a573
CassHsu <cass.hsu@gmail.com>
2023-01-18 23:13:41 +0800

    Update 2529-01.go

--- 2529/2529-01.go
+++ 2529/2529-01.go
@@ -2,8 +2,14 @@ func maximumCount(nums []int) int {
     p, n := 0, 0
 
     for _, num := range nums {
-        if num > 0 { p++ }
-        if num < 0 { n++ }
+        if num > 0 { 
+            p++
+            continue
+        }
+        if num < 0 { 
+            n++
+            continue
+        }
     }
 
     if p > n { return p }
