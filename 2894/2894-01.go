a9e78d9ca78081bae40ded7686690c3aff13e7db
CassHsu <cass.hsu@gmail.com>
2023-10-29 23:31:10 +0800

    Create 2894-01.go

---
+++ 2894/2894-01.go
@@ -0,0 +1,12 @@
+func differenceOfSums(n int, m int) int {
+    s1 := 0
+    s2 := 0
+    for i := 1; i <= n; i++ {
+        if i % m == 0 {
+            s2 += i
+        } else {
+            s1 += i
+        }
+    }
+    return s1 - s2
+}
