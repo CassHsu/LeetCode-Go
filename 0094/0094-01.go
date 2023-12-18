fb74e3605c049d69fca473e67c25c9a269d2e543
CassHsu <cass.hsu@gmail.com>
2023-12-18 23:26:35 +0800

    Update 0094-01.go

--- 0094/0094-01.go
+++ 0094/0094-01.go
@@ -4,17 +4,16 @@ type InOrder struct {
 
 func inorderTraversal(root *TreeNode) []int {
     inorder := InOrder { ans: []int{}}
-    inorder.traversal(root)
+    inorder.dfs(root)
     return inorder.ans
 }
 
-func (in *InOrder) traversal(node *TreeNode) {
+func (in *InOrder) dfs(node *TreeNode) {
     if node == nil {
         return
     }
     
-    in.traversal(node.Left)
+    in.dfs(node.Left)
     in.ans = append(in.ans, node.Val)
-    in.traversal(node.Right)
-    return
+    in.dfs(node.Right)
 }
